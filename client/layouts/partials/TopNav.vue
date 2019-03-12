3f74daa0b09fe92073d593c3a01c08e52484afb9
agit <kembareen@gmail.com>
2019-03-12 14:58:19 +0700

    #4 - Laravel + NuxtJS authentication: Setting up Nuxt

---
+++ client/layouts/partials/TopNav.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <div class="container">
+    <b-navbar type="light" variant="light">
+    <nuxt-link class="navbar-brand" to="/">Jwt-Authe</nuxt-link>
+
+    <b-navbar-nav>
+      <li class="nav-item">
+        <nuxt-link class="nav-link" to="/dashboard">Dashboard</nuxt-link> 
+      </li>
+    </b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
+      <li class="nav-item">
+        <nuxt-link class="nav-link" to="/dashboard">Login</nuxt-link> 
+      </li>
+      <li class="nav-item">
+        <nuxt-link class="nav-link" to="/dashboard">Register</nuxt-link>
+      </li>
+    </b-navbar-nav>
+    </b-navbar>
+    </div>
+
+</div>
+</template>
+
